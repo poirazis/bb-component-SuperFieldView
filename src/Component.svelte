30a762e3653913984f2444cb1486cd1aeb399fc8
poirazis <poirazis@gmail.com>
2023-05-24 14:41:57 +0300

    Release 1.0.0

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,16 +1,16 @@
 <script>
   import { getContext } from "svelte"
 
+  const { styleable } = getContext("sdk")
+  const component = getContext("component")
+
   export let heading
   export let headingSize
   export let headingWeight
   export let headingColor
-  export let headingVariant
-  export let subHeading
   export let hideHeading
   export let dataProvider
   export let fieldList
-  export let direction
   export let columns
   export let rowGap
   export let columnGap
@@ -21,57 +21,90 @@
   export let labelSize
   export let labelWeight
   export let labelColor
-  export let labelVariant
   export let valueSize
   export let valueColor
-  export let valueWeight
-  export let valueVariant
 
-  const { styleable } = getContext("sdk")
-  const component = getContext("component")
+  $: row = dataProvider?.rows?.length > 0 ? dataProvider.rows[0] : {}
 
-  $: row = dataProvider?.rows.length > 0 ? dataProvider.rows[0] : {}
+  // Add CSS variables to outer wrapper
+  $: $component.styles.normal = { 
+    ...$component.styles.normal, 
+    "--super-field-view-row-gap" : rowGap,
+    "--super-field-view-column-gap" : columnGap,
+    "--super-field-view-columns" : columns,
+    "--super-field-view-label-color" : labelColor,
+    "--super-field-view-value-color" : valueColor,
+    "--super-field-view-heading-color" : headingColor,
+    "--super-field-view-item-direction" : labelsAbove ? "column" : "row",
+    "--super-field-view-label-width" : labelWidth
+  } 
+
 </script>
 
 <div use:styleable={$component.styles}>
-  {#if !hideHeading}
-    <div 
-      class="super-field-view-heading spectrum-Heading spectrum-Heading--size{headingSize}"  
-      style:--super-field-view-heading-color={headingColor} > {heading} </div>
-  {/if}
-  <div class="superFieldView" 
-    style:--super-field-view-label-color={labelColor}
 
-    style:--super-field-view-row-gap={rowGap}
-    style:--super-field-view-column-gap={columnGap}
-    style:--super-field-view-columns={columns}>
+  {#if collapsible}
 
-    {#if !fieldList }
-      Please select some fields
-    {:else if !row}
-      No Data Found
-    {:else}
-      {#each fieldList as field, idx}
-      <div 
-        style:--super-field-view-item-direction={labelsAbove ? "column" : "row"}
-        class="superFieldViewItem">
-
-          <div 
-            style:--super-field-view-label-width={labelWidth}
-            class:spectrum-Heading--light={labelWeight=="light"}
-            class="superFieldViewItemLabel spectrum-Heading spectrum-Heading--size{labelSize}"> 
-            {field.displayName.toUpperCase()} 
+    <div class="spectrum-Accordion">
+      <div class:is-open={!collapsed} class="spectrum-Accordion-item">
+        <h3 class="spectrum-Accordion-itemHeading">
+          <button 
+            on:click={() => collapsed = !collapsed } 
+						class:spectrum-Heading--light={headingWeight == "light"}
+            class="spectrum-Accordion-itemHeader spectrum-Heading spectrum-Heading--size{headingSize}" type="button">{heading.toUpperCase()}
+          </button>
+          <svg
+          class="spectrum-Icon spectrum-UIIcon-ChevronRight100 spectrum-Accordion-itemIndicator" focusable="false" aria-hidden="true">
+            <use xlink:href="#spectrum-css-icon-Chevron100" />
+          </svg>
+        </h3>
+        <div class="spectrum-Accordion-itemContent">
+          <div class="superFieldView">
+            {#each fieldList as field, idx}
+              <div class="superFieldViewItem">
+        
+                  <div 
+                    class:spectrum-Heading--light={labelWeight=="light"}
+                    class="superFieldViewItemLabel spectrum-Heading spectrum-Heading--size{labelSize}"> 
+                    {field.displayName.toUpperCase()} 
+                  </div>
+        
+                  <div class="superFieldViewItemValue spectrum-Body spectrum-Body--size{valueSize}">
+                    { row[field.name] || "-" } 
+                  </div>
+        
+              </div>
+            {/each}
           </div>
-
-          <div class="superFieldViewItemValue spectrum-Body spectrum-Body--size{valueSize}">
-            { row[field.name] || "-" } 
-          </div>
-
+        </div>
       </div>
-      {/each}
-      <slot />
-    {/if}
-  </div>
+    </div>
+
+  {:else}
+	{#if !hideHeading}
+    <h3 class="spectrum-Heading--size{headingSize} super-field-view-heading" class:spectrum-Heading--light={headingWeight == "light"} >
+        {heading.toUpperCase()}
+    </h3>
+		{/if}
+		<div class="superFieldView">
+			{#each fieldList as field, idx}
+				<div class="superFieldViewItem">
+	
+						<div 
+							class:spectrum-Heading--light={labelWeight=="light"}
+							class="superFieldViewItemLabel spectrum-Heading spectrum-Heading--size{labelSize}"> 
+							{field.displayName.toUpperCase()} 
+						</div>
+	
+						<div class="superFieldViewItemValue spectrum-Body spectrum-Body--size{valueSize}">
+							{ row[field.name] || "-" } 
+						</div>
+	
+				</div>
+			{/each}
+		</div>
+
+  {/if}
 </div>
 
 <style>
@@ -98,22 +131,33 @@
   }
 
   .superFieldViewItemLabel {
-    min-width: var(--super-field-view-label-width);
-    color: var(--super-field-view-label-color);
     display: flex;
     flex-direction: column;
     justify-content: center;
     white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis !important;
+    min-width: var(--super-field-view-label-width);
+    color: var(--super-field-view-label-color);
   }
+
   .superFieldViewItemValue {
     width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis !important;
+    color: var(--super-field-view-value-color);
+  }
+
+	.spectrum-Accordion-itemContent {
+		padding-top: 0.85rem;
+	}
+
+  .spectrum-Accordion-itemHeading .spectrum-Accordion-itemHeader {
+    min-height: unset !important;
+		color: var(--super-field-view-heading-color)
+  }
+
+  .spectrum-Accordion-itemHeading .spectrum-Accordion-itemHeader:hover {
+		color: var(--spectrum-global-color-gray-900);
   }
 </style>
